2f3a230996a0331977b38c0ed83d6730c8cf93a0
krish588 <leelakrishna588@gmail.com>
2022-12-13 14:22:39 +0530

    Update train.py

--- gae/train.py
+++ gae/train.py
@@ -126,6 +126,7 @@ def get_roc_score(edges_pos, edges_neg, emb=None):
         neg.append(adj_orig[e[0], e[1]])
 
     preds_all = np.hstack([preds, preds_neg])
+    print(preds_all)
     labels_all = np.hstack([np.ones(len(preds)), np.zeros(len(preds_neg))])
     roc_score = roc_auc_score(labels_all, preds_all)
     ap_score = average_precision_score(labels_all, preds_all)
